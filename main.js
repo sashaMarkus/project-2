07504cfa647c765528c7695061873b9d55bd49b1
unknown <sashamarkus@hotmail.com>
2019-09-11 17:11:13 +0300

    more info

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 API's:
 https://api.coingecko.com/api/v3/coins/{id}
 */
-
-
-
-
 const app = {
     MAX_SECONDS: 120,
     newArrayOfCoins: [],
@@ -20,12 +16,12 @@ function allCoinsRequest() {
         mainBuild();
     } else {
         $.get('https://api.coingecko.com/api/v3/coins/list', (obj) => {
-            app.newArrayOfCoins = [...obj].slice(299, 399);
+            app.newArrayOfCoins = [...obj].slice(299, 599);
             app.coinId = 0;
             app.newArrayOfCoins.map((element) => {
                 element.coinId = app.coinId;
                 app.coinId++;
-
+                //console.log(element);
                 let coinObj = new Coin(element.coinId, element.symbol, element.name, false);
                 app.allCoinObjArray.push(coinObj);
             });
@@ -38,13 +34,12 @@ function allCoinsRequest() {
 
 function main() {
     allCoinsRequest();
-    $('#homeBtn').click(allCoinsRequest);
+    $('#homeBtn').click(mainBuild);
     $('.chosenCoinsBtn').click(chosenCoinsBtn);
 }
 
 function mainBuild() {
-    console.log(app.newArrayOfCoins);
-
+    emptyRow();
     app.newArrayOfCoins.forEach((element, idx) => {
         $('#row').append(`<div id="cardBody${idx}" class="card cardDiv col-md-2 mr-3 mt-3 "></div>`);
         $('#cardBody' + idx).append(`
@@ -54,43 +49,62 @@ function mainBuild() {
             </label>
             <h3>${element.symbol.toUpperCase()}</h3>
             <h7>${element.name}<br> <br></h7>
-            <button id="button${idx} class="info-btn primary-btn" data-toggle="collapse" data-target="#more-info-btn${idx}" data-coin-for-info="${element.id}">More info</button>
-            <div id="more-info-btn${idx}" class="collapse">
-                <div class="infoDropDown"></div>
-            </div>`);
+            <button id="button${idx}" class="btn-primary" data-coinid="${element.id}">More info</button>
+                <div class="infoDropDown" id="dropdown${element.coinId}"></div>`);
         $(`#toggleButton${element.coinId}`).click(toggleCoin);
         $(`#button${idx}`).click(moreInfoBtn);
     });
+
+    updateToggles();
 }
 
-function toggleCoin() {
-    let id = this.id.substr(12);
-    if (this.checked) { //check
-        for (let i = 0; i < app.allCoinObjArray.length; i++) {
-            if (id == app.allCoinObjArray[i].id) {
-                app.allCoinObjArray[i].checked = true;
-                let idx = app.coinsselectedArray.indexOf(app.allCoinObjArray[i]);
-                if (idx == -1) {
-                    app.coinsselectedArray.push(app.allCoinObjArray[i]);
-                    isCheckedFull();
-
+function toggleCoin() { 
+        let id = this.id.substr(12);
+        if (this.checked) { //check
+            for (let i = 0; i < app.allCoinObjArray.length; i++) {
+                if (id == app.allCoinObjArray[i].id) {
+                    app.allCoinObjArray[i].checked = true;
+                    let idx = app.coinsselectedArray.indexOf(app.allCoinObjArray[i]);
+                    if (idx == -1) {
+                        app.coinsselectedArray.push(app.allCoinObjArray[i]);
+                        isCheckedFull();
+    
+                    }
+                }
+            }
+        } else if (!this.checked) { //uncheck
+            for (let i = 0; i < app.allCoinObjArray.length; i++) {
+                if (id == app.allCoinObjArray[i].id) {
+                    app.allCoinObjArray[i].checked = false;
+                    let idx = app.coinsselectedArray.indexOf(app.allCoinObjArray[i]);
+                    app.coinsselectedArray.splice(idx, 1);
                 }
             }
         }
-    } else if (!this.checked) { //uncheck
-        for (let i = 0; i < app.allCoinObjArray.length; i++) {
-            if (id == app.allCoinObjArray[i].id) {
-                app.allCoinObjArray[i].checked = false;
-                let idx = app.coinsselectedArray.indexOf(app.allCoinObjArray[i]);
-                app.coinsselectedArray.splice(idx, 1);
+
+        let uncheckedCoins = [];
+        for(let i = 0; i < app.allCoinObjArray.length;i++){
+            if(!app.allCoinObjArray[i].checked){
+                 uncheckedCoins.push(app.allCoinObjArray[i]);
             }
         }
-    }
+
+        if(app.coinsselectedArray.length >=5){
+            for(let i = 0; i < uncheckedCoins.length; i++){
+                let toggleButton = document.getElementById('toggleButton' + uncheckedCoins[i].id);
+                toggleButton.disabled = true;
+            }
+        }else{
+            for(let i = 0; i < app.allCoinObjArray.length;i++){
+                let toggleButton = document.getElementById('toggleButton' + app.allCoinObjArray[i].id);
+                toggleButton.disabled = false;
+            }
+        }
+   
 }
 
 function isCheckedFull() {
-    if (app.coinsselectedArray.length > 5) {
-        console.log('fulll');
+    if (app.coinsselectedArray.length >= 5) {
         writePopUp();
     }
 }
@@ -149,7 +163,7 @@ function chosenCoinsBtn() {
     newRow.className = 'row';
     newRow.id = 'row';
     mainBuild.appendChild(newRow);
-    for (let i = 0; i < app.coinsselectedArray.length ; i++) {
+    for (let i = 0; i < app.coinsselectedArray.length; i++) {
         $('#row').append(`<div id="popup-cardBody${i}" class="card cardDiv col-md-2 mr-3 mt-3 ">
         <label class="switch" id="switch${app.coinsselectedArray[i].id}">
         <input id="popup-t${app.coinsselectedArray[i].id}" class="myToggleBtn" type="checkbox">
@@ -157,26 +171,63 @@ function chosenCoinsBtn() {
         </label>
         <h3>${app.coinsselectedArray[i].symbol.toUpperCase()}</h3>
         <h7>${app.coinsselectedArray[i].name}<br> <br></h7>
-        <button id="button${i} class="info-btn primary-btn" data-toggle="collapse" data-target="#more-info-btn${i}" data-coin-for-info="${app.coinsselectedArray[i].id}">More info</button>
-            <div id="more-info-btn${i}" class="collapse">
-                <div class="infoDropDown"></div>
-            </div>
+        <button id="button${app.coinsselectedArray[i].id}" class="btn-primary" data-coinid="${app.coinsselectedArray[i].id}>More info</button>
+                <div class="infoDropDown" id="dropdown${app.coinsselectedArray[i].id}"></div>
     </div>`);
+
+
     }
 
-    for(let i=0; i<app.coinsselectedArray.length; i++){
+    for (let i = 0; i < app.coinsselectedArray.length; i++) {
         let element = app.coinsselectedArray[i];
-        if(element.checked){
-            let toggle = document.getElementById('popup-t'+app.coinsselectedArray[i].id);
+        if (element.checked) {
+            let toggle = document.getElementById('popup-t' + app.coinsselectedArray[i].id);
+            toggle.addEventListener('click', toggleCoin);
             toggle.checked = true;
         }
     }
 }
+function updateToggles() {
+    if (app.coinsselectedArray.length > 0) {
+        let selectedIDS = [];
+        for (let i = 0; i < app.coinsselectedArray.length; i++) {
+            selectedIDS.push(app.coinsselectedArray[i].id);
+        }
 
-function moreInfoBtn() {
+        for (let i = 0; i < app.newArrayOfCoins.length; i++) {
+            if (selectedIDS.indexOf(app.newArrayOfCoins[i].coinId) != -1) {
+                let button = document.getElementById('toggleButton' + app.newArrayOfCoins[i].coinId);
+                button.checked = true;
+            }
+        }
+    }
+}
+function emptyRow() {
+    let newDiv = document.getElementById('row');
+    $(newDiv).empty();
+}
+
+async function moreInfoBtn() {
     let coinIdx = (this.id).substr(6);
-    let dataCoinForInfo = this.dataset.coinForInfo;
-    
+    let coinID = this.dataset.coinid;
+    let moreInfoDiv = document.getElementById('dropdown' + coinIdx);
+    if ($(moreInfoDiv).is(':empty')) {
+        $.getJSON('https://api.coingecko.com/api/v3/coins/' + coinID, (res) => {
+            moreInfoDiv.innerHTML = `
+            <img src="${res.image.small}"/> <br>
+            USD : ${res.market_data.current_price.usd} <br>
+            EUR : ${res.market_data.current_price.eur}<br>
+            ILS : ${res.market_data.current_price.ils}`;
+        });
+    } else {
+        $(moreInfoDiv).empty();
+    }
+
+}
+
+function callMoreInfo(id) {
+
+    return
 }
 
 
